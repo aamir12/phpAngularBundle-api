e36f4d66c3c355a542274aace1c07bdfb0fdd763
aamir12 <aamirkhan8878@gmail.com>
2021-03-14 17:02:02 +0530

    add googel logn setup

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
-    <meta name="google-signin-client_id
+    <meta name="google-signin-client_id"
       content="43829369774-pbtautnm3t9b05s5d2bi6ilel0uq8rsv.apps.googleusercontent.com"
     />
 	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
