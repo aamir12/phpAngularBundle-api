da92f34c1c44cff798ff9ecc3173e7fd938d5169
aamir12 <aamirkhan8878@gmail.com>
2021-03-14 16:53:29 +0530

    add googel logn setup

--- index.php
+++ index.php
@@ -9,75 +9,72 @@
       name="google-signin-client_id"
       content="43829369774-pbtautnm3t9b05s5d2bi6ilel0uq8rsv.apps.googleusercontent.com"
     />
-
-	<script
-	src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
-	<script src="https://apis.google.com/js/platform.js" async defer></script>
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
  </head>
-
-
-
 <body>
 
-<div > 
 
 <p>Today's welcome message is:</p>
-
-  <div class="g-signin2" data-onsuccess="onSignIn"></div>
- 
-
-  <button onclick="verify()">Verify Request</button>
-
-</div>
-
+<div id="my-signin2"></div>
+<button onclick="verify()">Verify Request</button>
 <script>
-$(document).ready(function(){
-    let data = {}
-    function onSignIn(googleUser) {
-	  var profile = googleUser.getBasicProfile();
-	  data = {
-	  	id:profile.getId(),
-	  	name: profile.getName(),
-	  	photo: profile.getImageUrl(),
-	  	email : profile.getEmail()
-	  }
-	  console.log(data);
-	  //call ajax;
-		  $.ajax({
-	        url: 'googleLogin.php?action=request',
-	        data: data,
-	        type: "POST",
-	        success: function (res, status) {
-	          console.log(`Success`);
-	          console.log(res);
-	        },
-	        error: function (xhr, status, error) {
-	          console.log(error);
-	        },
-	      });
-    }
+let data = {}
+function onSuccess(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  data = {
+  	id:profile.getId(),
+  	name: profile.getName(),
+  	photo: profile.getImageUrl(),
+  	email : profile.getEmail()
+  }
+  console.log(data);
+  //call ajax;
+	  $.ajax({
+        url: 'googleLogin.php?action=request',
+        data: data,
+        type: "POST",
+        success: function (res, status) {
+          console.log(`Success`);
+          console.log(res);
+        },
+        error: function (xhr, status, error) {
+          console.log(error);
+        },
+      });
 
-    function verify(){
-        $.ajax({
-	        url: 'googleLogin.php?action=verify',
-	        data: data,
-	        type: "POST",
-	        success: function (res, status) {
-	          console.log(`Success`);
-	          console.log(res);
-	        },
-	        error: function (xhr, status, error) {
-	          console.log(error);
-	        },
-	    });
-    }
+}
 
-});
+function onFailure(error) {
+  console.log(error);
+}
 
+function renderButton() {
+  gapi.signin2.render('my-signin2', {
+    'scope': 'profile email',
+    'width': 240,
+    'height': 50,
+    'longtitle': true,
+    'theme': 'dark',
+    'onsuccess': onSuccess,
+    'onfailure': onFailure
+  });
+}
 
-
+function verify(){
+    $.ajax({
+        url: 'googleLogin.php?action=verify',
+        data: data,
+        type: "POST",
+        success: function (res, status) {
+          console.log(`Success`);
+          console.log(res);
+        },
+        error: function (xhr, status, error) {
+          console.log(error);
+        },
+    });
+}
 </script>
-
+<script src="https://apis.google.com/js/platform.js?onload=renderButton" async defer></script>
 </body>
 </html>
